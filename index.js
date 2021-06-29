ef8f28e1b821b2692d37dd8935fa7d8bd8be1799
lucia7777 <38116582+qinatsu@users.noreply.github.com>
2021-06-29 18:09:53 -0500

    At least stable image upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 const DiscordRPC = require('discord-rpc');
+const sharp = require('sharp');
 const config = require('./config.json');
 const clientId = "858518597848268870";
 const user = 'tsuuuuki';
@@ -8,6 +9,18 @@ const audioScrobblerAPI = `http://ws.audioscrobbler.com/2.0/?method=user.getrece
 const audioScrobblerTrack = (artist, song) => `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${key}&artist=${encodeURIComponent(artist)}&track=${encodeURIComponent(song)}&format=json`
 const client = new DiscordRPC.Client({ transport: 'ipc' });
 let lastRPC = null;
+const headers = {
+    "content-type": "application/json",
+    "authorization": config.authorization
+}
+const fetchAssetsListHas = async (name) => {
+    let assetListReq = await fetch(`https://discord.com/api/v9/oauth2/applications/${clientId}/assets`, {
+        headers
+    })
+    let json = await assetListReq.json();
+    console.log(json.filter(asset => asset.name === name).length)
+    return json.filter(asset => asset.name === name.toLowerCase()).length === 0;
+}
 const update = () => {
     fetch(audioScrobblerAPI).then(res => res.json()).then(json => {
         
@@ -20,22 +33,47 @@ const update = () => {
                 //console.log(lastRPC)
                 console.log(`${json.recenttracks["@attr"].user} is scrobbling ${encodeURIComponent(artist)} - ${encodeURIComponent(name)}`)
                 if(lastRPC !== `${artist} ${name}`) {
-                    fetch(audioScrobblerTrack(artist, name)).then(res => res.json()).then(({ track }) => {
+                    fetch(audioScrobblerTrack(artist, name)).then(res => res.json()).then(async ({ track }) => {
                         if(!track) track = {} // lazy fuck
-                        lastRPC = `${artist} ${name}`; // lazy fuck
-                        client.request('SET_ACTIVITY', {
-			    pid: process.pid,
-			    activity: {
-                            	details: `${name} ${track.album ? track.album.title !== name ? `(from "${track.album.title}")` : "" : ""}`,
-                            	state: lastTrack.artist["#text"]
-			    },
-			    /*buttons: [
-			    	{ label: "View", url: lastTrack.url },
-				{ label: "User", url: `https://www.last.fm/user/${user}` }
-		            ]*/
+			lastRPC = `${artist} ${name}`; // lazy fuck - very importsantto have this before anything otherwirse bad thing will hapen )like uploading the same image 10 times)
+                        console.log(track.album)
+                        if(track.album) {
+                            let imgKey = track.album.title.slice(0,32).replace(/[^A-Za-z0-9]/g, "");
+                            let isnotAlreadyInAssetList = await fetchAssetsListHas(imgKey);
+                            console.log(isnotAlreadyInAssetList)
+                            if(isnotAlreadyInAssetList) {
+                                let extension = track.album.image[3]["#text"].split('.')
+                                extension = extension[extension.length-1];
+                                let imageReq = await fetch(track.album.image[3]["#text"]);
+                                let buffer = await imageReq.buffer()
+                                let img = await sharp(buffer)
+                                    .resize(512, 512).toBuffer();
+                                let base64 = `data:image/${extension};base64,${img.toString('base64')}`
+    
+                                let uploadImageReq = await fetch(`https://discord.com/api/v9/oauth2/applications/${clientId}/assets`, {
+                                    headers,
+                                    method: "POST",
+                                    body: JSON.stringify({
+                                        name: imgKey,
+                                        type: "1",
+                                        image: base64
+                                    })
+                                })
+                                let reqJson = await uploadImageReq.json();
+                                console.log(reqJson)
+                            } else {
+                                console.log('Is already in assets list')
+                            }
                             
-                            
-                        });
+                        }
+                        
+                        
+                        client.setActivity({
+                            details: `${name} ${track.album ? track.album.title !== name ? `(from "${track.album.title}")` : "" : ""}`,
+                            state: lastTrack.artist["#text"],
+                            largeImageKey: track.album ? track.album.title.slice(0,32).replace(/[^A-Za-z0-9]/g, "").toLowerCase() : "",
+                            largeImageText: track.album ? track.album.title : ""
+                        }).then(w => console.log(w))
                         console.log("Updated presence")
                     })
                 } else {
